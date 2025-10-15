0a8ea96a227dd6c1d0f33bdc6c5d73fc5677d22d
gatewaykishore <gateway.kishorekanna@gmail.com>
2025-10-15 17:58:47 +0530

    Updated slots based on payment copies from users

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
